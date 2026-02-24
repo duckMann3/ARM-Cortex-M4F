9950dac86b53e5fe15132d09ae20c7bec87fe1e3
David <David.Ca3545@gmail.com>
2026-02-24 12:03:14 -0800

    Fixed header include for gpio file, added main function to compile in Keil uVision, added startup.s, and added gitignore to remove unnecessary build files

--- joystick/src/gpio.c
+++ joystick/src/gpio.c
@@ -1,16 +1,15 @@
-#include "tm4c123gh6pm.h"
+#include "../inc/tm4c123gh6pm.h"
 
-#define ENABLE_PB6      0x40
-#define 
+#define ENABLE_PB6      0x40 
 
 void PortB_Init(void) {
-  SYSCTL_RCGCGPIO_R1 |= ENABLE_PB6;  
-  while((SYSCTL_RCGCGPIO_R1&ENABLE_PB6) != 0) {}
+  SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;  
+  while((SYSCTL_RCGCGPIO_R&SYSCTL_RCGCGPIO_R1) != SYSCTL_RCGCGPIO_R1) {}
 
   GPIO_PORTB_PCTL_R  &=  0xFF000000;     // Set GPIO Function
   GPIO_PORTB_AMSEL_R &= ~ENABLE_PB6;     // Disable Analog Function
   GPIO_PORTB_DIR_R   |=  ENABLE_PB6;     // Set Output
-  GPIO_PORTB_AFSEL   &= ~ENABLE_PB6;     // Regular Port Function
+  GPIO_PORTB_AFSEL_R &= ~ENABLE_PB6;     // Regular Port Function
   GPIO_PORTB_DEN_R   |=  ENABLE_PB6;     // Set Digital Enable PB6
   GPIO_PORTB_PUR_R   |=  ENABLE_PB6;     // Set Pull Up Resistor
 }
